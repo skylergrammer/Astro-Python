3bf5ef1e4532b3b06a3394fc09a89e10883b8cbc
Skyler <skylergrammer@gmail.com>
2014-06-25 10:31:24 -0500

    no overlap necessary

--- stitch_spec.py
+++ stitch_spec.py
@@ -7,22 +7,23 @@ import pyfits
 import argparse
 from scipy.ndimage.filters import gaussian_filter1d
 from scipy.interpolate import *
-
+import sys
 
 def readSpectrum(filename):
   '''
   Read in a spectrum.  Return wavelengths, intensities, and header.
   '''
   data, header = pyfits.getdata(filename, header=True)
+  if len(data) != int(header['naxis1']): data = data[0]
 
   lambda1 = header['crval1']
   lambda2 = float(header['naxis1'])*header['cdelt1']+float(header['crval1'])
   
   # Convert pixels to angstroms
   wave = np.arange(lambda1, lambda2, header['cdelt1'])
-  wave = wave[:len(data[0])]
+  wave = wave[:len(data)]
 
-  return (wave, data[0], header)
+  return (wave, data, header)
 
 
 def smoothSpec(spectrum):
@@ -81,9 +82,9 @@ def stitchSpectra(wave1, spectrum1, wave2, spectrum2, grid=0.25):
   waves = np.array(list(wave1) + list(wave2))
   spec = np.array(list(spectrum1) + list(spectrum2))
 
-  stitch_spec = np.array([np.mean(spec[np.where(waves == x)]) for x in all_wave])
+  #stitch_spec = np.array([np.mean(spec[np.where(waves == x)]) for x in all_wave])
 
-  return (all_wave, stitch_spec)
+  return (waves, spec)
   
 
 def writeSpectrum(wave, spectrum, header, grid=0.25):
@@ -94,7 +95,50 @@ def writeSpectrum(wave, spectrum, header, grid=0.25):
   header.update(CRVAL1=min(wave))
   header.update(CDELT1=grid)
   
-  pyfits.writeto('spec_'+header['object']+'.fits', spectrum, header=header)
+  pyfits.writeto('spec_'+header['object']+'.fits', spectrum, header=header, clobber=True)
+
+
+def run(filename1, filename2):
+
+  # Read in spectrum 1 and then smooth it
+  wave1, spectrum1, header1 = readSpectrum(filename1)
+  smooth_spectrum1 = smoothSpec(spectrum1)
+  
+  # Read in spectrum 2 and then smooth it
+  wave2, spectrum2, header2 = readSpectrum(filename2)
+  smooth_spectrum2 = smoothSpec(spectrum2)
+
+  plt.plot(wave1, smooth_spectrum1, 'k')
+  plt.plot(wave2, smooth_spectrum2, 'r')
+  plt.xlim(3500, 8500)
+  plt.show()
+
+  # Verify that the two spectra are the same object
+  if header1['object'] != header2['object']:
+    sys.exit('ERROR: Input spectra are not the same object!')
+
+  # Determine the vertical offset between the overlapping regions
+  #offset = findOffset(wave1, smooth_spectrum1, wave2, smooth_spectrum2)
+
+  # Subtract the offset between overlapping region
+  #mod_spectrum2 = np.array(spectrum2)+offset
+  
+  # Determine the interpolation resolution: uses the highest resolution between the spectra
+  interp_bin = np.round(min([header1['cdelt1'], header1['cdelt1']]),1)
+ 
+  # Interpolate both spectra to the same resolution
+  interp_wave1, interp_spectrum1 = interpSpec(wave1, spectrum1, grid=interp_bin)
+  interp_wave2, interp_spectrum2 = interpSpec(wave2, spectrum2, grid=interp_bin)
+
+  # Stitch together the spectra
+  x,y = stitchSpectra(interp_wave1, interp_spectrum1, 
+                      interp_wave2, interp_spectrum2, grid=interp_bin) 
+
+  # Write stitched spectrum to file
+  try:
+    writeSpectrum(x,y, header1, grid=interp_bin)
+  except:
+    sys.exit('Cannot write to file.  Verify that output filename does not already exist.')
 
 
 def main():
@@ -103,39 +147,21 @@ def main():
                                    spectra.  Use this program if the overlapping \
                                    region is small compared to the full wavelength \
                                    range.')
-  parser.add_argument('spec1', help='Filename for spectrum 1.')
-  parser.add_argument('spec2', help='Filename for spectrum 2.')
+  parser.add_argument('--blue', help='Filename for blue spectrum.')
+  parser.add_argument('--red', help='Filename for red spectrum.')
+  parser.add_argument('--list', help='Two-column list of spectra to be combined. \
+                                     Each row must correspond to a single object')
   args = parser.parse_args()
 
-
-  # Read in spectrum 1 and then smooth it
-  wave1, spectrum1, header1 = readSpectrum(args.spec1)
-  smooth_spectrum1 = smoothSpec(spectrum1)
+  if args.list:
+    f = open(args.list, 'r')
+    for line in f:
+      blue, red = line.split()
+      print 'Combining %s with %s' % (blue, red)
+      run(blue, red)
+  else:
+    run(args.blue, args.red)
   
-  # Read in spectrum 2 and then smooth it
-  wave2, spectrum2, header2 = readSpectrum(args.spec2)
-  smooth_spectrum2 = smoothSpec(spectrum2)
-
-  # Determine the vertical offset between the overlapping regions
-  offset = findOffset(wave1, smooth_spectrum1, wave2, smooth_spectrum2)
-
-  # Subtract the offset between overlapping region
-  mod_spectrum1 = spectrum1-offset
-  
-  # Determine the interpolation resolution: uses the highest resolution between the spectra
-  interp_bin = np.round(min([header1['cdelt1'], header1['cdelt1']]),1)
- 
-  # Interpolate both spectra to the same resolution
-  interp_wave1, interp_spectrum1 = interpSpec(wave1, mod_spectrum1, grid=interp_bin)
-  interp_wave2, interp_spectrum2 = interpSpec(wave2, spectrum2, grid=interp_bin)
-
-  # Stitch together the spectra
-  x,y = stitchSpectra(interp_wave1, interp_spectrum1, 
-                      interp_wave2, interp_spectrum2, grid=interp_bin) 
-
-  # Write stitched spectrum to file
-  writeSpectrum(x,y, header1, grid=interp_bin)
-
 
 if __name__ == '__main__':
   main()

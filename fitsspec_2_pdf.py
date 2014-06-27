b81d3a11dddf35233d943ac2398f179c3241af27
Skyler Grammer <skylergrammer@gmail.com>
2014-06-27 16:49:04 -0500

    now will make row stacked spectra

--- fitsspec_2_pdf.py
+++ fitsspec_2_pdf.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 from scipy.ndimage.filters import *
-from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from matplotlib.ticker import MultipleLocator
 
 
 def getSpectrum(filename):
@@ -31,60 +31,86 @@ def smoov(data, size):
   return smooved_data
 
 
-def plotSpec(wave, data, header, bounds):
+def plotSpec(wave, data, header, bounds, fig, ax, number):
 
   median_y = np.median(data[np.where((wave > bounds[0]) & (wave <= bounds[1]))])
   max_y = max(data[np.where((wave > bounds[0]) & (wave <= bounds[1]))])
   min_y = min(data[np.where((wave > bounds[0]) & (wave <= bounds[1]))])
   std_y = np.std(data[np.where((wave > bounds[0]) & (wave <= bounds[1]))])
 
-
-  fig, ax = plt.subplots(figsize=(15,5), dpi=72)
-  fig.subplots_adjust(wspace=0.2, left=0.025, right=0.975,
-                        bottom=0.2, top=0.92)
-  
-
-  plt.plot(wave, data, 'k', lw=1.5, label=header['object'])
+  # Plot the spectrum
+  ax[number].plot(wave, data, 'k', lw=1, label=header['object'])
   
   # x and y limits
-  plt.ylim(median_y - std_y, median_y + std_y)
-  plt.xlim(bounds[0], bounds[1])
-  
-  # x-tick major and minor locations at 500 and 100 Angstroms
-  #xmajorLocator = MultipleLocator(500)
-  #xminorLocator = MultipleLocator(100)
-  #ax.xaxis.set_major_locator(xmajorLocator)
-  #ax.xaxis.set_minor_locator(xminorLocator)
-  
-  # Suppress ticks on y axiss
-  plt.tick_params(axis='x', which='both', labelsize=22)
-  ax.tick_params(axis='y', which='both', left='off',right='off', labelleft='off')
 
-  # Set x label
-  plt.xlabel('$\lambda$ [$\AA$]', fontsize=30)
-  ax.xaxis.labelpad = 10
+  if max_y > median_y+4*std_y:
+    ax[number].set_ylim(min_y, 2*np.abs(median_y))
+  else:
+    ax[number].set_ylim(min_y, max_y)
   
-  # Set title
-  plt.title(header['object'], fontsize=30)
-  plt.show()
+  ax[number].set_xlim(bounds[0], bounds[1])
+  
 
 def main():
 
   parser = argparse.ArgumentParser()
   parser.add_argument('input', nargs='+')
-  parser.add_argument('--bounds', nargs=2, type=float, required=True)
+  parser.add_argument('--bounds', metavar=('Blue','Red'), nargs=2, type=float, required=True)
   parser.add_argument('--smooth', type=int, default=2)
+  parser.add_argument('--ydim', metavar='Rows', type=int, default=1)
 
   args = parser.parse_args()
 
+  xmajorLocator = MultipleLocator(500)
+  xminorLocator = MultipleLocator(100)
 
+  ax_count = 0
   for each in args.input:
 
+    # If restarting: initialize plot    
+    if ax_count == 0:
+      fig, ax = plt.subplots(nrows=args.ydim, ncols=1, sharex=True, sharey=False,
+                             figsize=(15,2*args.ydim), dpi=72)
+      fig.subplots_adjust(hspace=0, left=0.025, right=0.975,
+                          bottom=0.1, top=0.99)
+      
+    # Read in spectrum
     wave, data, header = getSpectrum(each)
+    # Smooth spectrum
     smooved_data = smoov(data, args.smooth)
+    # Plot spectrum
+    plotSpec(wave, smooved_data, header, args.bounds, fig, ax, ax_count)
+    ax[ax_count].legend(frameon=False, loc='upper left', markerscale=None) 
+    ax[ax_count].xaxis.set_major_locator(xmajorLocator)
+    ax[ax_count].xaxis.set_minor_locator(xminorLocator)
 
-    plotSpec(wave, smooved_data, header, args.bounds)
+    # If bottom plot, make the xlabel
+    if ax_count == args.ydim-1:
+      ax[ax_count].tick_params(axis='y', which='both', left='off',right='off', labelleft='off')
+      ax[ax_count].tick_params(axis='x', which='both', labelsize=22)
+      ax[ax_count].set_xlabel('$\lambda$ [$\AA$]', fontsize=30)
+      ax[ax_count].xaxis.labelpad = 10
+    
+    # If not the bottom plot, suppress all labels
+    else: 
+      ax[ax_count].tick_params(axis='both', which='both', 
+                               bottom='on', top='off', left='off',right='off', 
+                               labelleft='off', labelbottom='off')
 
+    # If ax_count is less than the number of rows, increase value by 1
+    if ax_count < args.ydim-1:
+      ax_count += 1
+    # If ax_count equal to number of rows, reset it and show the final plot
+    else: 
+      ax_count = 0
+      plt.show()
+
+  '''  
+  plt.savefig('spec_'+header['object']+'.pdf', 
+                  dpi=None, facecolor='w', edgecolor='w',
+                  transparent=True, bbox_inches=None, pad_inches=0.1,
+                  frameon=None)
+  '''
 if __name__ == '__main__':
   main()
   

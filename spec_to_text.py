2a890ef1508e56e0f7ba298cc1b364a00a4408e1
Skyler Grammer <skylergrammer@gmail.com>
2014-07-23 09:41:50 -0500

    added smoothing capabilities

--- spec_to_text.py
+++ spec_to_text.py
@@ -4,8 +4,10 @@ import pyfits
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter1d
 
-def readSpectrum(filename):
+
+def readSpectrum(filename, smooth=False):
     '''
     Read in a spectrum.  Return wavelengths, intensities, and header.
     '''
@@ -14,7 +16,11 @@ def readSpectrum(filename):
         data = data[0]
 
     waves = np.linspace(header['crval1'],header['naxis1']*header['cdelt1']+header['CRVAL1'], num=header['naxis1'])
-   
+
+    if smooth:
+      data = gaussian_filter1d(data, sigma=3.0)
+      print 'Smoothed'
+
     return (waves, data, header)
 
 
@@ -22,7 +28,7 @@ def main():
     parser = argparse.ArgumentParser(description='Copy first extension of input files to target directory.')
     
     parser.add_argument('filelist',nargs='+',help='List of files with 1D spectra.')
-
+    parser.add_argument('--s', action='store_true', default=False, help='If set, does a 3 pixel Gaussian smooth.')
     parser.add_argument('-outdir',default='.',help="Target directory to copy files (default = './')")
 
 
@@ -30,7 +36,7 @@ def main():
 
     fcounts = 0
     for filename in args.filelist:
-        waves,data,hdr = readSpectrum(filename)
+        waves,data,hdr = readSpectrum(filename, smooth=args.s)
         
         #outname
         outname = os.path.splitext(filename)[0] + '.txt'
